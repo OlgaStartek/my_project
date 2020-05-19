43a3b24d1a7c61f4ac695c5639292d265a1ccf62
OlgaStartek <64373744+OlgaStartek@users.noreply.github.com>
2020-05-20 00:11:46 +0200

    Create game.cpp

---
+++ game.cpp
@@ -0,0 +1,27 @@
+#include "game.h"
+
+//private functions:
+void Game::initVariables(){
+
+}
+
+void Game::initWindow(){
+
+}
+//constructors/destructors:
+game::Game()
+{
+    //constructor
+}
+
+game::~Game()
+{
+    //destructor
+}
+//functions
+void Game::update(){
+
+}
+void Game::render(){ //visual representation
+
+}
