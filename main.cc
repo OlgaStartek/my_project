9c92e8e22f06a64b3653f4826aa46f31d420b992
OlgaStartek <64373744+OlgaStartek@users.noreply.github.com>
2020-05-20 00:10:35 +0200

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,26 +1,31 @@
 #include <iostream>
+#include "game.h"
 
-class Game {
-  public:
-    static void Start();
-  private:
-    enum GameState {
-        Uninitialized,
-        Pause,
-        Playing,
-        Exiting
-    };
-  inline static GameState _gameState = Uninitialized;
-};
+int main(){
+    //window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My game", sf::Style::Titlebar | sf::Style::Close); //mamy miejsce tytulu i zamkniecie (nie pozwalam na resize)
+    sf::Event event;
+    //game loop
+    while(window.isOpen()){
+        //events
+        while(window.pollEvent(event)){ //jak dlugo dostajemy informacje z okna wpisujemy pod event
+            switch(event.type){
+            case sf::Event::Closed:
+                window.close(); //zamykamy jesli kliknieto przycisk close
+                break;
+            case sf::Event::Keypressed:
+                if(event.key.code == sf::Keyboard::Escape)
+                    window.close();
+                break;
+            }
+        }
+        //update
 
-void Game::Start() {
-    if(_gameState != Game::Uninitialized) return;
-    std::cout << "Init ...\n";
-    _gameState = Game::Playing;
-}
-
-int main()
-{
-    Game::Start();
+        //render
+        window.clear(sf::Color::Cyan); //zerujemy to co bylo
+        //main game
+        window.display(); //skonczylismy
+    }
+    //end
     return 0;
 }
