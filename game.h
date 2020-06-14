1a40e51f37c745267cf01802a067914cf3f708e2
OlgaStartek <64373744+OlgaStartek@users.noreply.github.com>
2020-06-14 18:37:05 +0200

    Update game.h

--- game.h
+++ game.h
@@ -2,11 +2,14 @@
 #define GAME_H
 
 #include <iostream>
+#include <vector>
+#include <ctime>
 
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/Audio.hpp>
+
 
 class Game {
 private:
@@ -15,7 +18,16 @@ private:
 	sf::VideoMode videoMode;
 	sf::Event event;
 
+	//Mouse position
+	sf::Vector2i mousePosWindow;
+
+	int points;
+	float spawnTimer;
+	float spawnTimerMax;
+	int maxKeys; //max na ekranie w tym samym czasie 
+
 	//Game objects
+	std::vector<sf::RectangleShape> keys;
 	sf::RectangleShape key;
 
 	//Private functions
@@ -31,8 +43,12 @@ public:
 	const bool running() const;
 	
 	//Functions
+	void spawn();
 	void pollEvents();
+	void updateMousePositions();
+	void updateKeys();
 	void update();
+	void renderKeys(); 
 	void render();
 
 
