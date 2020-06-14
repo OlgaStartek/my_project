9c2de9d76e4ae6e37773051cddcffede32013092
OlgaStartek <64373744+OlgaStartek@users.noreply.github.com>
2020-06-14 18:36:12 +0200

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -2,6 +2,11 @@
 
 void Game::initVariables(){
 	this->window = nullptr;
+
+    this->points = 0;
+    this->spawnTimerMax = 1000.f;
+    this->spawnTimer = this->spawnTimerMax;
+    this->maxKeys = 5;
 }
 
 void Game::initWindow(){
@@ -9,7 +14,7 @@ void Game::initWindow(){
 	this->videoMode.width = 800;
 	this->window = new sf::RenderWindow(this->videoMode, "My game", sf::Style::Titlebar | sf::Style::Close);
 	
-    this->window->setFramerateLimit(144);
+    this->window->setFramerateLimit(60);
 }
 
 void Game::initKeys(){
@@ -33,7 +38,21 @@ const bool Game::running() const
 {
 	return this->window->isOpen();
 }
+
 //Functions
+void Game::spawn(){
+    /* spawn keys
+    set their color and position
+    */
+    this->key.setPosition(
+        static_cast<float>(rand() % static_cast<int>(this->window->getSize().x - this->key.getSize().x)),
+        0.f); //zaczynamy od samej gory
+    this->key.setFillColor(sf::Color::Black);
+    //spawn
+    this->keys.push_back(this->key);
+
+}
+
 void Game::pollEvents(){
     //Event poll
     while (window->pollEvent(event)) {
@@ -49,11 +68,48 @@ void Game::pollEvents(){
     }
 }
 
+void Game::updateMousePositions(){
+    //pozycja myszy wzgledem okna gryv
+    this->mousePosWindow = sf::Mouse::getPosition(*this->window);
+}
+
+void Game::updateKeys(){
+    /* update spawn timer
+    spawn keys
+    moves keys downwords
+    usunac na dole okna
+    */
+    if (this->keys.size() < this->maxKeys) {
+        
+        if (this->spawnTimer >= this->spawnTimerMax) {
+            //spawn and reset
+            this->spawn();
+            this->spawnTimer = 0.f;
+        }
+        else
+            this->spawnTimer += 1.f;
+    }
+
+    //move
+    for (auto &k : this->keys) {
+        k.move(0.f, 2.f); //przesuwamy w dol
+    }
+}
+
 void Game::update(){
+
     this->pollEvents();
 
-    //pozycja myszy wzgledem okna gry
-    std::cout << "Mouse pos: " << sf::Mouse::getPosition(*this->window).x << " " << sf::Mouse::getPosition(*this->window).y << "\n";;
+    this->updateMousePositions();
+
+    this->updateKeys();
+}
+
+void Game::renderKeys(){
+    //rendering
+    for (auto &k : this->keys) {
+        this->window->draw(k);
+    }
 }
 
 void Game::render(){
@@ -63,9 +119,9 @@ void Game::render(){
     -display
     */
 
-    window->clear(sf::Color::Cyan);
+    this->window->clear(sf::Color::Cyan);
 
-    window->draw(this->key);
+    this->renderKeys();
 
-    window->display();
+    this->window->display();
 }
