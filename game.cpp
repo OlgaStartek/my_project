cc61dd616f3800233c95fca9725fa48787df81f1
OlgaStartek <64373744+OlgaStartek@users.noreply.github.com>
2020-06-14 15:35:25 +0200

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,27 +1,71 @@
 #include "game.h"
 
-//private functions:
 void Game::initVariables(){
-
+	this->window = nullptr;
 }
 
 void Game::initWindow(){
+	this->videoMode.height = 600;
+	this->videoMode.width = 800;
+	this->window = new sf::RenderWindow(this->videoMode, "My game", sf::Style::Titlebar | sf::Style::Close);
+	
+    this->window->setFramerateLimit(144);
+}
+
+void Game::initKeys(){
+    this->key.setPosition(10.f, 10.f);
+    this->key.setSize(sf::Vector2f(100.f, 100.f)); //dwa floaty
+    this->key.setFillColor(sf::Color::Black);
+}
+
+Game::Game() {
+	this->initVariables();
+	this->initWindow();
+    this->initKeys();
+}
+
+Game::~Game() {
+	delete this->window;
 
 }
-//constructors/destructors:
-game::Game()
+//Accessors
+const bool Game::running() const
 {
-    //constructor
+	return this->window->isOpen();
+}
+//Functions
+void Game::pollEvents(){
+    //Event poll
+    while (window->pollEvent(event)) {
+        switch (event.type) {
+        case sf::Event::Closed:
+            window->close();
+            break;
+        case sf::Event::KeyPressed:
+            if (event.key.code == sf::Keyboard::Escape)
+                window->close();
+            break;
+        }
+    }
 }
 
-game::~Game()
-{
-    //destructor
-}
-//functions
 void Game::update(){
+    this->pollEvents();
 
+    //pozycja myszy wzgledem okna gry
+    std::cout << "Mouse pos: " << sf::Mouse::getPosition(*this->window).x << " " << sf::Mouse::getPosition(*this->window).y << "\n";;
 }
-void Game::render(){ //visual representation
 
+void Game::render(){
+    /*
+    -clear old
+    -render objects
+    -display
+    */
+
+    window->clear(sf::Color::Cyan);
+
+    window->draw(this->key);
+
+    window->display();
 }
